291b892a77a584e1ddc1b44b77f23f4c6f1ee7f1
ManikyaAlister <alisterm@student.unimelb.edu.au>
2024-01-19 11:41:12 +1100

    various analyses including adding broad claim type

--- analyses/04_combine_output_group.R
+++ analyses/04_combine_output_group.R
@@ -6,8 +6,10 @@ models <- c("group-prior", "group-prior-consensus", "group-prior-consensus-claim
 excluded_conditions <- c("contested", "dependent")
 
 model_details <- expand.grid(model = models, excluded_condition = excluded_conditions)
+
 all_output <- NULL
 all_looic <- NULL
+all_se <- NULL
 for ( i in 1:length(model_details[,1])){
   print(i)
   output <- NULL
@@ -17,9 +19,28 @@ for ( i in 1:length(model_details[,1])){
   load(here(paste0("analyses/02_output/",model,"-rm-",excluded_condition,".Rdata")))
   all_output[[i]] = list(model, excluded_condition, output)
   looic <- loo(output)$estimates["looic","Estimate"]
+  se <- loo(output)$estimates["looic","SE"]
+  all_se[i] <- se
+  all_looic[i] <- looic
+}
+
+model_LOOICs <- cbind(model_details, all_looic, all_se)
+
+save(all_looic,all_output, model_LOOICs, file = here("data/derived/group_output_combined.Rdata"))
+
+# for broad claim types
+all_looic <- NULL
+for ( i in 1:length(model_details[,1])){
+  print(i)
+  output <- NULL
+  looic <- NULL
+  model <- model_details[i,"model"]
+  excluded_condition <- model_details[i,"excluded_condition"]
+  load(here(paste0("analyses/02_output/broad-",model,"-rm-",excluded_condition,".Rdata")))
+  looic <- loo(output)$estimates["looic","Estimate"]
   all_looic[i] <- looic
 }
 
 model_LOOICs <- cbind(model_details, all_looic)
 
-save(all_looic,all_output, model_LOOICs, file = here("data/derived/group_output_combined.Rdata"))
+save(model_LOOICs, file = here("data/derived/broad_group_output_combined.Rdata"))
