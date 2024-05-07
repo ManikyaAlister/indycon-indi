e190c9fa87f580d27e1e32f9c22b2f591ed0adcf
ManikyaAlister <alisterm@student.unimelb.edu.au>
2024-05-07 18:30:35 +1000

    fixing combined group output scripts

--- analyses/04_combine_output_group.R
+++ analyses/04_combine_output_group.R
@@ -2,19 +2,18 @@ rm(list = ls())
 library(here)
 library(brms)
 
-models <- c("group-prior", "group-prior-consensus", "group-prior-consensus-claim", "group-prior-consensusXclaim")
+
+original_models <- c("group-prior", "group-prior-consensus", "group-prior-consensus-claim", "group-prior-consensusXclaim")
+follow_up_models <- c("group-prior-consensusXclaim-certainty", "group-prior-consensusXclaimXsource")
+models <- c(original_models, follow_up_models)
+is_follow_up <- models %in% follow_up_models # label whether it's an original or follow up model
 excluded_conditions <- c("contested", "dependent")
 
 model_details <- expand.grid(model = models, excluded_condition = excluded_conditions)
-<<<<<<< HEAD
 
 all_output <- NULL
 all_looic <- NULL
 all_se <- NULL
-=======
-all_output <- NULL
-all_looic <- NULL
->>>>>>> de0e661 (by session models)
 for ( i in 1:length(model_details[,1])){
   print(i)
   output <- NULL
@@ -24,13 +23,12 @@ for ( i in 1:length(model_details[,1])){
   load(here(paste0("analyses/02_output/",model,"-rm-",excluded_condition,".Rdata")))
   all_output[[i]] = list(model, excluded_condition, output)
   looic <- loo(output)$estimates["looic","Estimate"]
-<<<<<<< HEAD
   se <- loo(output)$estimates["looic","SE"]
   all_se[i] <- se
   all_looic[i] <- looic
 }
 
-model_LOOICs <- cbind(model_details, all_looic, all_se)
+model_LOOICs <- cbind(model_details, all_looic, all_se, is_follow_up)
 
 save(all_looic,all_output, model_LOOICs, file = here("data/derived/group_output_combined.Rdata"))
 
@@ -44,15 +42,9 @@ for ( i in 1:length(model_details[,1])){
   excluded_condition <- model_details[i,"excluded_condition"]
   load(here(paste0("analyses/02_output/broad-",model,"-rm-",excluded_condition,".Rdata")))
   looic <- loo(output)$estimates["looic","Estimate"]
-=======
->>>>>>> de0e661 (by session models)
   all_looic[i] <- looic
 }
 
 model_LOOICs <- cbind(model_details, all_looic)
 
-<<<<<<< HEAD
 save(model_LOOICs, file = here("data/derived/broad_group_output_combined.Rdata"))
-=======
-save(all_looic,all_output, model_LOOICs, file = here("data/derived/group_output_combined.Rdata"))
->>>>>>> de0e661 (by session models)
