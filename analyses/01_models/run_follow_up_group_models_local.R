b1e83beb8859de47b25f5bd3418b941573607255
ManikyaAlister <alisterm@student.unimelb.edu.au>
2024-10-14 15:44:14 +1100

    RR1 analyses first draft

--- analyses/01_models/run_follow_up_group_models_local.R
+++ analyses/01_models/run_follow_up_group_models_local.R
@@ -8,7 +8,8 @@ library(brms, lib.loc = lib)
 
 
 # define model name 
-models <- c("group-prior-consensusXclaim-certainty", "group-prior-consensusXclaimXsource")
+models <- c("group-prior-consensus*age")
+  #c("group-prior-consensusXclaim-certainty", "group-prior-consensusXclaimXsource")
 
 # how to run wirh commandArgs: type in terminal 
 # Rscript analyses/01_models/run_follow_up_group_models_local.R  <model e.g., "group-prior-consensusXclaimXsource">
@@ -16,21 +17,50 @@ model <- commandArgs(trailingOnly = TRUE)
 
 # load participant data
 load(here(paste0("data/clean/all_data_clean.Rdata")))
-data <- all_data[[1]]
+data_reponses <- all_data$data
+demographics <- all_data$demographics
 
+# join demographic data with response data
+data <- full_join(data_reponses, demographics, by = "ANON_PID") %>%
+  select(-participant.y) %>%
 # add transformed prior that captures prior certainty
-data <- data %>% 
-  mutate(pre_certainty = abs(pre_adjusted - 50))
+  mutate(participant = participant.x,
+    pre_certainty = abs(pre_adjusted - 50),
+    demographics_age = as.numeric(demographics_age)
+  )
 
-conditions_to_remove <- c("contested", "dependent")
+
+conditions_to_remove <- c("dependent", "contested")
 
 
 for (remove in conditions_to_remove){
   filtered_data <- data[data[,"consensus"] != remove,]
    if (model == "group-prior-consensusXclaim-certainty"){
      output <- brm(post_adjusted ~ (1|participant) + pre_adjusted + pre_certainty + consensus * claim_type, data = filtered_data)
+   }  else if (model == "group-prior-consensusXclaim-stanceclaim"){
+     output <- brm(post_adjusted ~ (1|participant) + pre_adjusted + original_stance_claim + consensus * claim_type, data = filtered_data)
+   } else if (model == "group-prior-consensusXclaim-certainty-stanceclaim"){
+     output <- brm(post_adjusted ~ (1|participant) + pre_adjusted + original_stance_claim + pre_certainty + consensus * claim_type, data = filtered_data)
    } else if (model == "group-prior-consensusXclaimXsource"){
      output <- brm(post_adjusted ~ (1|participant) + pre_adjusted + consensus * claim_type * source, data = filtered_data)
+   } else if (model == "group-prior-consensus*age"){
+     output <- brm(post_adjusted ~ (1|participant) + pre_adjusted + consensus * demographics_age, data = filtered_data)
+   } else if (model == "group-prior-consensus*politcalstrength") {
+     filtered_data_pol <- filtered_data %>%
+       filter(! demographics_politics %in% c("soc", "anarch")) # these don't fall as nicely in the storng-lib to stron-conservative scale (even though one could argue thay are strong lib)
+     output <- brm(post_adjusted ~ (1|participant) + pre_adjusted + consensus * political_strength, data = filtered_data_pol)
+   }else if (model == "group-prior-consensus*politcalscale") {
+     filtered_data_pol <- filtered_data %>%
+       filter(! demographics_politics %in% c("soc", "anarch")) 
+     output <- brm(post_adjusted ~ (1|participant) + pre_adjusted + consensus * political_scale, data = filtered_data_pol)
+   } else if (model == "group-prior-consensus*politcalgroup") {
+     output <- brm(post_adjusted ~ (1|participant) + pre_adjusted + consensus * political_group, data = filtered_data)
+   } else if (model == "group-prior-consensus*unieducation") {
+     output <- brm(post_adjusted ~ (1|participant) + pre_adjusted + consensus * university_education, data = filtered_data)
+   } else if (model == "group-prior-consensus*socmedprop") {
+     output <- brm(post_adjusted ~ (1|participant) + pre_adjusted + consensus * socmed_proportionate, data = filtered_data)
+   } else if (model == "changed-mind") {
+     output <- brm(changed_mind ~ pre_certainty + consensus * claim_type, family = bernoulli(), data = filtered_data, cores = parallel::detectCores()/2)
    }
   
   save(output, model, remove, file = here(paste0("analyses/02_output/",model,"-rm-",remove,".Rdata")))
